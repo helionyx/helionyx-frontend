5f0a35cafd96f09b173ce4a977e4f4879dddad06
khaoxkongz <makkawal.thanapat@gmail.com>
2024-07-27 19:10:13 +0700

    🎈 perf: biome format import

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -11,104 +11,104 @@
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
-import { Route as AboutImport } from './routes/about'
-import { Route as ContactImport } from './routes/contact'
-import { Route as IndexImport } from './routes/index'
 import { Route as ProductsImport } from './routes/products'
-import { Route as ProductsProductIdImport } from './routes/products.$productId'
+import { Route as ContactImport } from './routes/contact'
+import { Route as AboutImport } from './routes/about'
+import { Route as IndexImport } from './routes/index'
 import { Route as ProductsIndexImport } from './routes/products.index'
+import { Route as ProductsProductIdImport } from './routes/products.$productId'
 
 // Create/Update Routes
 
 const ProductsRoute = ProductsImport.update({
-	path: '/products',
-	getParentRoute: () => rootRoute,
+  path: '/products',
+  getParentRoute: () => rootRoute,
 } as any)
 
 const ContactRoute = ContactImport.update({
-	path: '/contact',
-	getParentRoute: () => rootRoute,
+  path: '/contact',
+  getParentRoute: () => rootRoute,
 } as any)
 
 const AboutRoute = AboutImport.update({
-	path: '/about',
-	getParentRoute: () => rootRoute,
+  path: '/about',
+  getParentRoute: () => rootRoute,
 } as any)
 
 const IndexRoute = IndexImport.update({
-	path: '/',
-	getParentRoute: () => rootRoute,
+  path: '/',
+  getParentRoute: () => rootRoute,
 } as any)
 
 const ProductsIndexRoute = ProductsIndexImport.update({
-	path: '/',
-	getParentRoute: () => ProductsRoute,
+  path: '/',
+  getParentRoute: () => ProductsRoute,
 } as any)
 
 const ProductsProductIdRoute = ProductsProductIdImport.update({
-	path: '/$productId',
-	getParentRoute: () => ProductsRoute,
+  path: '/$productId',
+  getParentRoute: () => ProductsRoute,
 } as any)
 
 // Populate the FileRoutesByPath interface
 
 declare module '@tanstack/react-router' {
-	interface FileRoutesByPath {
-		'/': {
-			id: '/'
-			path: '/'
-			fullPath: '/'
-			preLoaderRoute: typeof IndexImport
-			parentRoute: typeof rootRoute
-		}
-		'/about': {
-			id: '/about'
-			path: '/about'
-			fullPath: '/about'
-			preLoaderRoute: typeof AboutImport
-			parentRoute: typeof rootRoute
-		}
-		'/contact': {
-			id: '/contact'
-			path: '/contact'
-			fullPath: '/contact'
-			preLoaderRoute: typeof ContactImport
-			parentRoute: typeof rootRoute
-		}
-		'/products': {
-			id: '/products'
-			path: '/products'
-			fullPath: '/products'
-			preLoaderRoute: typeof ProductsImport
-			parentRoute: typeof rootRoute
-		}
-		'/products/$productId': {
-			id: '/products/$productId'
-			path: '/$productId'
-			fullPath: '/products/$productId'
-			preLoaderRoute: typeof ProductsProductIdImport
-			parentRoute: typeof ProductsImport
-		}
-		'/products/': {
-			id: '/products/'
-			path: '/'
-			fullPath: '/products/'
-			preLoaderRoute: typeof ProductsIndexImport
-			parentRoute: typeof ProductsImport
-		}
-	}
+  interface FileRoutesByPath {
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexImport
+      parentRoute: typeof rootRoute
+    }
+    '/about': {
+      id: '/about'
+      path: '/about'
+      fullPath: '/about'
+      preLoaderRoute: typeof AboutImport
+      parentRoute: typeof rootRoute
+    }
+    '/contact': {
+      id: '/contact'
+      path: '/contact'
+      fullPath: '/contact'
+      preLoaderRoute: typeof ContactImport
+      parentRoute: typeof rootRoute
+    }
+    '/products': {
+      id: '/products'
+      path: '/products'
+      fullPath: '/products'
+      preLoaderRoute: typeof ProductsImport
+      parentRoute: typeof rootRoute
+    }
+    '/products/$productId': {
+      id: '/products/$productId'
+      path: '/$productId'
+      fullPath: '/products/$productId'
+      preLoaderRoute: typeof ProductsProductIdImport
+      parentRoute: typeof ProductsImport
+    }
+    '/products/': {
+      id: '/products/'
+      path: '/'
+      fullPath: '/products/'
+      preLoaderRoute: typeof ProductsIndexImport
+      parentRoute: typeof ProductsImport
+    }
+  }
 }
 
 // Create and export the route tree
 
 export const routeTree = rootRoute.addChildren({
-	IndexRoute,
-	AboutRoute,
-	ContactRoute,
-	ProductsRoute: ProductsRoute.addChildren({
-		ProductsProductIdRoute,
-		ProductsIndexRoute,
-	}),
+  IndexRoute,
+  AboutRoute,
+  ContactRoute,
+  ProductsRoute: ProductsRoute.addChildren({
+    ProductsProductIdRoute,
+    ProductsIndexRoute,
+  }),
 })
 
 /* prettier-ignore-end */

7a53f7bc5d2d7e672311fc4e4c682cfca7b7e9d1
khaoxkongz <makkawal.thanapat@gmail.com>
2024-07-28 01:55:14 +0700

    🐞 fix: use devtool on development only

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,8 @@
 import type { QueryClient } from '@tanstack/react-query'
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRouteWithContext<{
 	queryClient: QueryClient
 }>()({
-	component: () => (
-		<>
-			<Outlet />
-			<TanStackRouterDevtools />
-		</>
-	),
+	component: () => <Outlet />,
 })

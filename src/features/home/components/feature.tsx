1866018760f4b84dee6f847e73d45adcbae8f231
nopre8888 <nanaka3@hotmail.com>
2024-08-26 23:18:07 +0700

    🦄 refactor: update Qr Footer contact

--- src/features/home/components/feature.tsx
+++ src/features/home/components/feature.tsx
@@ -13,7 +13,7 @@ const Feature: React.FC = () => {
 		<section className='py-24 bg-gray-50'>
 			<div className='container mx-auto px-4'>
 				<h2 className='text-4xl font-bold text-center mb-16 text-[#F89D44]'>{t('whyChooseUs')}</h2>
-				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
+				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6'>
 					<div className='bg-red-800 p-6 rounded-lg shadow-lg flex flex-col items-center'>
 						<img src={unmatchPrecision} alt='Reliability' className='h-20 w-auto mb-4' />
 						<h3 className='text-2xl font-medium text-white mb-2'>{t('feature.reliability.title')}</h3>
@@ -34,12 +34,10 @@ const Feature: React.FC = () => {
 						<h3 className='text-2xl font-medium text-white mb-2'>{t('feature.innovation.title')}</h3>
 						<p className='text-white text-center'>{t('feature.innovation.description')}</p>
 					</div>
-					<div className='col-span-full flex justify-center mt-6'>
-						<div className='bg-red-800 p-6 rounded-lg shadow-lg flex flex-col items-center max-w-sm'>
-							<img src={quality} alt='Quality' className='h-20 w-auto mb-4' />
-							<h3 className='text-2xl font-medium text-white mb-2'>{t('feature.quality.title')}</h3>
-							<p className='text-white text-center'>{t('feature.quality.description')}</p>
-						</div>
+					<div className='bg-red-800 p-6 rounded-lg shadow-lg flex flex-col items-center'>
+						<img src={quality} alt='Quality' className='h-20 w-auto mb-4' />
+						<h3 className='text-2xl font-medium text-white mb-2'>{t('feature.quality.title')}</h3>
+						<p className='text-white text-center'>{t('feature.quality.description')}</p>
 					</div>
 				</div>
 			</div>
